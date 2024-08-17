558b7536b4940b3d382e3e57e915a2b74aad921d
Steau Vlad <vlad.steau11@gmail.com>
2024-08-17 15:28:41 +0300

    First part of exercices component

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -7,9 +7,14 @@ import {exerciseOptions, fetchData } from '../utils/fetchData'
 function Exercises({exercices, setExercices, bodyPart}) {
   
   return (
-    <>
-    <div>Exercises</div>
-    </>
+    <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
+      <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
+      <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
+        {exercices.map((exercise, idx) => (
+          <p>{exercise.name}</p>
+        ))}
+      </Stack>
+    </Box>
   )
 }
 
